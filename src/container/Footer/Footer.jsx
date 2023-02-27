3d0f6753eb70aaa58193c92af0f47f2bfa7083cf
alecchhung <aleclavie@proton.me>
2023-02-27 17:00:32 +0800

    reversed

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -39,7 +39,7 @@ const Footer = () => {
     <>
       <h2 className="head-text">Drop me a line</h2>
 
-      {/* <div className="app__footer-cards">
+      <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
           <a href="mailto:noreply@alec.com" className="p-text">noreply@alec.com</a>
@@ -48,7 +48,7 @@ const Footer = () => {
           <img src={images.mobile} alt="phone" />
           <a href="tel:+49 (123) 456-7890" className="p-text">+49 (123) 456-7890</a>
         </div>
-      </div> */}
+      </div>
       {!isFormSubmitted ? (
         <div className="app__footer-form app__flex">
           <div className="app__flex">
