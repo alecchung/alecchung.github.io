33dc0027b7696a51d65cc8714dfc4e1781f162c7
alecchung <aleclavie@proton.me>
2023-03-14 22:47:34 +0800

    Categories

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -41,7 +41,7 @@ const Work = () => {
       <h2 className="head-text">My Creative <span>Projects</span></h2>
 
       <div className="app__work-filter">
-        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+        {['MERN', 'Full Stack', 'Frontend', 'Backend', 'TypeScript', 'All'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
