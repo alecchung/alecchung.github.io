8add8bd8e59f3f16a702b1dd435dac855e704b12
alecchung <aleclavie@proton.me>
2023-03-30 09:59:08 +0800

    desc

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => (
         <div className="badge-cmp app__flex">
           <span>👋</span>
           <div style={{ marginLeft: 20 }}>
-            <p className="p-text">Hello, this is </p>
+            <p className="p-text">Hi there, this is </p>
             <h1 className="head-text">Alec</h1>
           </div>
         </div>
