d962600bcf2088158f4413fd6f3fd16e3df910ea
alecchung <aleclavie@proton.me>
2023-03-30 13:02:46 +0800

    src

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,22 +2,27 @@ import about01 from '../assets/about01.png';
 import about02 from '../assets/about02.png';
 import about03 from '../assets/about03.png';
 import about04 from '../assets/about04.png';
+import angular from '../assets/angular.svg'
 import api from '../assets/api.png';
 import bgIMG from '../assets/bgIMG.png';
 import bgWhite from '../assets/bgWhite.png';
+import bootstrap from '../assets/bootstrap.svg';
 import circle from '../assets/circle.svg';
-import cpp from '../assets/c++.svg';
+import cpp from '../assets/cpp.svg';
 import css from '../assets/css.svg';
 import email from '../assets/email.png';
+import expressjs from '../assets/expressjs.svg';
 import figma from '../assets/figma.png';
 import flutter from '../assets/flutter.svg';
 import git from '../assets/git.svg';
 import graphql from '../assets/graphql.svg';
 import html from '../assets/html.svg';
 import javascript from '../assets/javascript.svg';
+import materialui from '../assets/materialui.svg';
 import mobile from '../assets/mobile.png';
-import mu5 from '../assets/material-ui.svg';
-import node from '../assets/nodejs.svg';
+import mongodb from '../assets/mongodb.svg';
+import nextjs from '../assets/nextjs.svg';
+import nodejs from '../assets/nodejs.svg';
 import profile from '../assets/profile.png';
 import python from '../assets/python.svg';
 import react from '../assets/react.svg';
@@ -31,22 +36,27 @@ const images = {
   about02,
   about03,
   about04,
+  angular,
   api,
   bgIMG,
   bgWhite,
+  bootstrap,
   circle,
   cpp,
   css,
   email,
+  expressjs,
   figma,
   flutter,
   git,
   graphql,
   html,
   javascript,
+  materialui,
   mobile,
-  mu5,
-  node,
+  mongodb,
+  nextjs,
+  nodejs,
   profile,
   python,
   react,
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -58,7 +58,7 @@ const Header = () => (
       whileInView={scaleVariants.whileInView}
       className="app__header-circles"
     >
-      {[images.html, images.react, images.css].map((circle, index) => (
+      {[images.react, images.html, images.css, images.javascript].map((circle, index) => (
         <div className="circle-cmp app__flex" key={`circle-${index}`}>
           <img src={circle} alt="profile_bg" />
         </div>
